2d8caa015c11c1c74f4e7e27226eafb9109f991b
yashwant solanki <134097007+yash-s01@users.noreply.github.com>
2025-06-04 17:57:28 +0530

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const AxiosInstance = axios.create({
-  baseURL: `http://${process.env.REACT_APP_BASE_URL}:5000`,
+  baseURL: `https://${process.env.REACT_APP_BASE_URL}`,
   headers: {
     'Content-Type': 'application/json',
   },
